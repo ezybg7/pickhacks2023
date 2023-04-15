c154d45c47a3563ff88d4ab50a1f6eff8045f6a9
Claire DeVlieger <ccd5p9@mst.edu>
2023-04-15 03:28:46 -0500

    grocery map has a title now

--- pantry/src/pages/GroceryMap.js
+++ pantry/src/pages/GroceryMap.js
@@ -4,6 +4,7 @@ import Map from '../components/Map'
 const GroceryMap = () => {
     return (
         <div>
+            <h2>Grocery stores in the area:</h2>
             <div className='map'>
                 <Map/>
             </div>
