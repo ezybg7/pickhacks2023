77ffb97fc7f3b3cada8e50c29eb78e4ad1942ca0
ezybg7 <ezybg7@umsystem.edu>
2023-04-15 11:36:15 -0500

    added dependencies to server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (req,res)=>{
+    res.send('hello me')
+})
+
+app.listen(3001, () => {
+    console.log("running on port 3001")
+})
