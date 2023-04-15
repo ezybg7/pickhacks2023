52b5d7650ab883238d2a0a2b08a987ff5f7d959f
yfu03 <yannisfu@gmail.com>
2023-04-15 11:49:33 -0500

    login style xd

--- pantry/src/pages/Login.js
+++ pantry/src/pages/Login.js
@@ -6,11 +6,16 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
-        <div className = "inputStyles">
+        <div className = "container">
+          <div className = "inputStyles">
             <h2>Email:</h2>
             <LogInInput value = {email} onChange = {setEmail}/>
+            
+          </div>
+          <div className = "inputStyles">
             <h2>Password:</h2>
             <LogInInput value = {password} onChange = {setPassword}/>
+          </div>
         </div>
     );
 }
