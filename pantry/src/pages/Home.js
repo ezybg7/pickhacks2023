bff965f8a4d098560c1eba2806d6531fc61b8116
Claire DeVlieger <ccd5p9@mst.edu>
2023-04-15 01:54:39 -0500

    Added summary on main page (with links)

--- pantry/src/pages/Home.js
+++ pantry/src/pages/Home.js
@@ -1,11 +1,15 @@
-import '../styles/Home.css'
 
+import '../styles/Cover.css';
 const Home = () => {
+
 
   return ( 
     <div className = "home">
-      <h1>Homepage!</h1>
-      
+      <h2>Homepage!</h2>
+      <h3>900 million tons of food worldwide are wasted each year, according to a global survey conducted by the UN Environment Program (<a href="https://www.bbc.com/news/science-environment-56271385">reported by BBC</a>). 
+        This is not only wasting valuable resources, but also contributing to environmental issues such as pollution and greenhouse gas emissions. As of 2021, around 830 
+        million people around the world struggle with food security and hunger (<a href="https://www.who.int/news/item/06-07-2022-un-report--global-hunger-numbers-rose-to-as-many-as-828-million-in-2021">reported by the UN</a>). Reducing the rate of food waste would help to 
+        alleviate this crisis and lessen the impact of food shortages.</h3>
     </div>
    );
 }
