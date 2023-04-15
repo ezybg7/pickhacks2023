dffa69d008fa4dbe2bc24cf993ae744495800fb3
Claire DeVlieger <ccd5p9@mst.edu>
2023-04-15 03:10:09 -0500

    map (not styled)

--- pantry/src/pages/GroceryMap.js
+++ pantry/src/pages/GroceryMap.js
@@ -4,6 +4,10 @@ const GroceryMap = () => {
     return (
         <div>
             <h1>HELLO</h1>
+            <iframe src="https://storage.googleapis.com/maps-solutions-if4yzf7a95/neighborhood-discovery/8h1m/neighborhood-discovery.html"
+            width="100%" height="100%"
+            loading="lazy">
+            </iframe>
         </div>
     );
 }
