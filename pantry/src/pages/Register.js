df75fae8aae4e7d0c97f40f2146c77d92511c308
ezybg7 <ezybg7@umsystem.edu>
2023-04-15 10:57:49 -0500

    Updated Register.js

--- pantry/src/pages/Register.js
+++ pantry/src/pages/Register.js
@@ -1,9 +1,16 @@
+import LogInInput from '../components/LogInInput';
 import '../styles/Register.css'
+import {useState} from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div>
-
+            <h2>Email:</h2>
+            <LogInInput value = {email} onChange = {setEmail}/>
+            <h2>Password:</h2>
+            <LogInInput value = {password} onChange = {setPassword}/>
         </div>
     );
 }
